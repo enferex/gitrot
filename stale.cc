89676b8c1dd6fce4c1154dc022c45081edc811d2
Matt Davis <mattdavis9@gmail.com>
2015-08-02 23:28:50 -0700

    Block parsing

--- stale.cc
+++ stale.cc
@@ -16,7 +16,13 @@ using std::ostream;
 using std::string;
 using std::vector;
 
+#define COMMENT_BLOCK_BEGIN "/*"
+#define COMMENT_BLOCK_END   "*/"
+
 class Block;
+class Line;
+typedef vector<Line *> Lines;
+typedef Lines::iterator LinesItr;
 
 class Line
 {
@@ -24,6 +30,8 @@ public:
     // Parsing utils
     static string getTextLine(FILE *fp);
     static Line *getLine(FILE *fp);
+    static bool isBlank(LinesItr &itr);
+    static bool hasUnterminatedComment(LinesItr &itr);
 
 private:
     // git help blame 
@@ -39,15 +47,12 @@ private:
     string _summary;
     string _prev_or_boundary;
     string _filename;
-    string _contents;
+    string _content;
 
     friend ostream &operator<<(ostream &os, const Line &tf) {
-        return os << tf._contents;
+        return os << tf._content;
     }
 };
-
-typedef vector<Line *> Lines;
-typedef Lines::iterator LinesItr;
 
 class Block
 {
@@ -59,6 +64,10 @@ private:
     Lines _lines;
 };
 typedef vector<Block *> Blocks;
+
+class BlankBlock : public Block
+{
+};
 
 class CommentBlock : public Block
 {
@@ -128,14 +137,85 @@ Line *Line::getLine(FILE *fp)
     ln->_summary          = getTextLine(fp);
     ln->_prev_or_boundary = getTextLine(fp);
     ln->_filename         = getTextLine(fp);
-    ln->_contents         = getTextLine(fp);
+    ln->_content          = getTextLine(fp);
 
     return ln;
 }
 
+// Start of a comment block
+bool Line::hasUnterminatedComment(LinesItr &itr)
+{
+    string content = (*itr)->_content;
+    bool in_comment = false;
+    size_t pos;
+
+    while ((pos = content.findCommentBlockBegin(pos)) != string::npos) {
+        in_comment = true;
+        if ((pos = content.findCommentBlockEnd(pos)))
+          in_comment = false;
+    }
+
+    return in_comment;
+}
+
+// Just spaces
+bool Line::isBlank(LinesItr &itr)
+{
+    for (auto i=itr->_content.cbegin(); i!=itr->_content.cend(); ++i)
+      if (!isspace(*i))
+        return false;
+
+    return true;
+}
+
+// Return true if no code exists in this line
+bool Line::justComment(LinesItr &itr)
+{
+    string content = (*itr)->_content;
+
+    size_t pos = 0;
+    while ((pos = content.find_first_not_of(" \n\t\r"), pos) != string::npos) {
+        auto begin = findCommentBegin(content.cbegin() + pos);
+        if (begin == pos) {
+            pos = findCommentEnd(content.cbegin() + pos);
+            pos += 2; // Skip */ (two chars)
+        }
+        else
+          return false;
+    }
+
+    return true;
+}
+
+// Continusly read lines grouping based on
+// comment and code.
 Block *TranslationFile::createBlock(LinesItr &itr)
 {
-    return NULL;
+    Block *block = NULL;
+
+    if (isBlank(itr)) {
+        block = new BlankBlock();
+        while (isBlank(itr)) {
+            block->push_back(*itr);
+            ++itr;
+        }
+    }
+    else if (hasUnterminatedComment(itr)) {
+        block = new CommentBlock();
+        while (!findCommentBlockEnd(itr)) {
+            block->push_back(*itr);
+            ++itr;
+        }
+    }
+    else {
+        block = new CodeBlock();
+        while (!findCommentBlockBegin(itr)) {
+            block->push_back(*itr);
+            ++itr;
+        }
+    } 
+
+    return block;
 }
 
 void TranslationFile::parse(ifstream &fs)
@@ -201,8 +281,6 @@ int main(int argc, char **argv)
     for_each(files.cbegin(), files.cend(),
              [](TranslationFile t) { cout << t << endl; });
 #endif
-
-
 
     return 0;
 }
